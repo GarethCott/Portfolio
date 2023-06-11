5995cf4855d043b5568cf4d3ede6ea97cf81919e
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-11 17:53:47 +0200

    Adding MongoDB and creating our API to fetch data from database

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import {notFound} from "next/navigation"
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
 
@@ -23,22 +23,22 @@ const BlogPost = async ({params}) => {
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Cotti</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            src={data.img}
             alt=""
             fill={true}
             className={styles.image}
@@ -47,7 +47,7 @@ const BlogPost = async ({params}) => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-        Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Aliquet risus feugiat in ante. Eget dolor morbi non arcu risus quis. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Amet justo donec enim diam. Malesuada fames ac turpis egestas. Faucibus ornare suspendisse sed nisi lacus. Eu sem integer vitae justo eget magna fermentum iaculis eu. Morbi enim nunc faucibus a pellentesque. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Tellus integer feugiat scelerisque varius morbi enim nunc.
+          {data.content}
         </p>
       </div>
     </div>
