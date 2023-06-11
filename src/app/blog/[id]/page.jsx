a207e8877abaa78e0f6f28e4ff972dd2f4339c14
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-11 16:21:49 +0200

    Playing around with the different ways to fetch data. Server, Client and Locally

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 import styles from "./page.module.css";
 import React from 'react'
+import {notFound} from "next/navigation"
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound()
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({params}) => {
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Consectetur a erat nam at lectus urna duis convallis.</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
           </p>
