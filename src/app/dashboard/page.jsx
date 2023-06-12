23377b2474d06c2d8610c0a652cd2d874b01c4fe
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-12 11:25:37 +0200

    Added routing protection

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './page.module.css'
 import useSWR from "swr";
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
   //OLD WAY TO FETCH DATA
@@ -32,8 +33,8 @@ const Dashboard = () => {
 
   const session = useSession()
 
-  console.log(session)
-  
+  const router = useRouter()
+
   //NEW WAY TO FETCH DATA
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -42,9 +43,17 @@ const Dashboard = () => {
     fetcher
   );
 
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
-}
+  if (session.status === "loading"){
+    return <p>Loading...</p>
+  }
+
+  if (session.status === "unauthenticated"){
+    router?.push("/dashboard/login")
+  }
+
+  if (session.status === "authenticated"){
+    return <div className={styles.container}>Dashboard</div>
+  }
+};
 
 export default Dashboard
